ee46b056fb84f729f0b5479e7350c3818d9e96c0
arifa mujawar <arifa>
2021-06-17 21:07:11 +0300

    feat: fetch events

---
+++ src/services/Events.ts
@@ -0,0 +1,35 @@
+import Axios from 'axios'
+
+export class EventService {
+  constructor() {}
+  public async fetchAllEvents(limit, start) {
+    const result = {
+      nextStartIndex: Number(limit) + Number(start),
+      data: [],
+    }
+
+    const url = `https://open-api.myhelsinki.fi/v1/events/`
+    const eventsData = await Axios.get(url, {
+      params: {
+        limit,
+        start,
+      },
+    })
+
+    const arrayOfEvents = eventsData.data.data
+    arrayOfEvents.forEach(event => {
+      const startDate = event.event_dates.starting_day.split('T')
+      const endDate = event.event_dates.ending_day.split('T')
+
+      const specificEvent = {
+        name: event.name['fi'],
+        info: event['info_url'],
+        description: event.description,
+        event_dates: startDate[0] + ' ' + endDate[0],
+      }
+      result.data.push(specificEvent)
+    })
+
+    return result
+  }
+}
